5f48afd9bd23c8fb54c5e359ae49f354c2f999ea
Yordy Castillo <u00030588@sise.com.pe.com>
2024-12-04 21:21:36 -0500

    Primer Ejercicio del Examen Parcial

---
+++ src/main/java/com/example/EPejer1Castillo/EPejer1CastilloApplication.java
@@ -0,0 +1,13 @@
+package com.example.EPejer1Castillo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EPejer1CastilloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EPejer1CastilloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/EPejer1Castillo/EPejer1CastilloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EPejer1Castillo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EPejer1CastilloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
